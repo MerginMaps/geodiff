9dc758c3dc16fa8a9c48995104e4329b8650f6bc
Peter Petrik <zilolv@gmail.com>
2019-07-09 16:46:07 +0200

    fix win

--- scripts/ci/windows/install.bash
+++ scripts/ci/windows/install.bash
@@ -2,6 +2,4 @@
 set -e
 
 choco install miniconda3
-ls -la C:/tools/miniconda3/condabin
-
-C:/tools/miniconda3/condabin/conda.bat install -c anaconda -y boost
+C:/tools/miniconda3/condabin/conda.bat install -c conda-forge boost
