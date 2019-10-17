995114f2ad3af79102ba752ad5bcf4a085490964
Peter Petrik <zilolv@gmail.com>
2019-10-17 14:01:44 +0200

    another try for release

--- scripts/ci/publish_pypi.bash
+++ scripts/ci/publish_pypi.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PWD=`pwd`
 
@@ -11,12 +13,12 @@ cd $DIR/../..
 $DIR/clean.bash
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-    python3 setup.py sdist bdist_wheel
+   python3 setup.py sdist bdist_wheel
 else
    python3 setup.py bdist_wheel
 fi
 
 # upload to testpypi
-twine upload dist/* -r "$URL" --username "__token__" --password "$PYPI_TOKEN"
+twine upload  dist/* --username "__token__" --password "$PYPI_TOKEN" -r "$URL"
 
 cd $PWD
--- scripts/ci/windows/install.bash
+++ scripts/ci/windows/install.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 choco install python
-python get-pip.py
+pip3 install --upgrade setuptools pip
 pip3 install twine scikit-build wheel cmake
