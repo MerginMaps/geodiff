c9276f9c9d54bdf5a6f357effdada1d5b3d8c5d2
Peter Petrik <zilolv@gmail.com>
2019-10-17 17:39:09 +0200

    another try

--- scripts/ci/windows/install.bash
+++ scripts/ci/windows/install.bash
@@ -3,18 +3,17 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# https://chocolatey.org/packages/python3
-# python 3.8
-choco install python3 --version=3.8.0
-C:/Python38/python.exe $DIR/../get-pip.py
-C:/Python38/Scripts/pip.exe install setuptools twine scikit-build wheel cmake
+# python 3.6
+choco install python3 --version=3.6.8 --force
+C:/Python36/python.exe $DIR/../get-pip.py
+C:/Python36/Scripts/pip.exe install setuptools twine scikit-build wheel cmake
 
 # python 3.7
-choco install python3 --version=3.7.5
+choco install python3 --version=3.7.5 --force
 C:/Python37/python.exe $DIR/../get-pip.py
 C:/Python37/Scripts/pip.exe install setuptools twine scikit-build wheel cmake
 
-# python 3.6
-choco install python3 --version=3.6.8
-C:/Python36/python.exe $DIR/../get-pip.py
-C:/Python36/Scripts/pip.exe install setuptools twine scikit-build wheel cmake
+# python 3.8
+choco install python3 --version=3.8.0 --force
+C:/Python38/python.exe $DIR/../get-pip.py
+C:/Python38/Scripts/pip.exe install setuptools twine scikit-build wheel cmake
