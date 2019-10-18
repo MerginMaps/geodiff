98e74d92dbe20646d8ec21a84d4f6f782db72df7
Peter Petrik <zilolv@gmail.com>
2019-10-18 12:49:18 +0200

    win32

--- scripts/ci/windows/build_wheel.bash
+++ scripts/ci/windows/build_wheel.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-for EXE in "C:/Python36/python.exe" "C:/Python37/python.exe" "C:/Python38/python.exe"
+for EXE in "C:/Python36/python.exe" "C:/Python37/python.exe" "C:/Python38/python.exe" "C:/Python36_32b/python.exe" "C:/Python37_32b/python.exe" "C:/Python38_32b/python.exe"
 do
     echo "$EXE"
     $EXE setup.py bdist_wheel
--- scripts/ci/windows/install.bash
+++ scripts/ci/windows/install.bash
@@ -18,13 +18,20 @@ if [ -n "$TRAVIS_TAG" ]; then
     choco install python3 --version=3.8.0 --force
     C:/Python38/python.exe $DIR/../get-pip.py
     C:/Python38/Scripts/pip.exe install setuptools twine scikit-build wheel cmake
-fi
 
-    ### 32 bit
-    choco install python3 --version=3.6.8 -forcex86 --params "/InstallDir:C:\Python37_32b" --force
+    ### 32 bit python 3.6
+    choco install python3 --version=3.6.8 --x86 --params "/InstallDir:C:\Python36_32b" --force
+    C:/Python36_32b/python.exe $DIR/../get-pip.py
+    C:/Python36_32b/Scripts/pip.exe install setuptools twine scikit-build wheel cmake
+
+    # 32 bit python 3.7
+    choco install python3 --version=3.7.5 --x86 --params "/InstallDir:C:\Python37_32b" --force
     C:/Python37_32b/python.exe $DIR/../get-pip.py
     C:/Python37_32b/Scripts/pip.exe install setuptools twine scikit-build wheel cmake
 
-    cd $DIR/../../..
-    C:/Python37_32b setup.py bdist_wheel
-    ls -la dist/
+    # 32 bit python 3.8
+    choco install python3 --version=3.8.0 --x86 --params "/InstallDir:C:\Python38_32b" --force
+    C:/Python38_32b/python.exe $DIR/../get-pip.py
+    C:/Python38_32b/Scripts/pip.exe install setuptools twine scikit-build wheel cmake
+
+fi
