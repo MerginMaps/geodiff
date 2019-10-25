7e5b401dd890bbf208c6f67ffbb68084e56d2edc
Peter Petrik <zilolv@gmail.com>
2019-10-25 14:56:05 +0200

    ver++

--- geodiff/src/geodiff.cpp
+++ geodiff/src/geodiff.cpp
@@ -23,7 +23,7 @@
 
 const char *GEODIFF_version()
 {
-  return "0.7.4";
+  return "0.7.5";
 }
 
 void _errorLogCallback( void *pArg, int iErrCode, const char *zMsg )
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import find_packages
 #from setuptools import setup
 from skbuild import setup
 
-VERSION = '0.7.4'
+VERSION = '0.7.5'
 
 setup(
     name="pygeodiff",
