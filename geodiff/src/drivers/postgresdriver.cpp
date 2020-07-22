e46b2ac8063e597ab202ce15b3c7dfdf7e745c45
Martin Dobias <wonder.sk@gmail.com>
2020-07-22 15:38:09 +0200

    Fix date/time compatibility between sqlite and postgres drivers

--- geodiff/src/drivers/postgresdriver.cpp
+++ geodiff/src/drivers/postgresdriver.cpp
@@ -331,6 +331,12 @@ static std::string allColumnNames( const TableSchema &tbl, const std::string &pr
 
     if ( c.isGeometry )
       columns += "ST_AsBinary(" + name + ")";
+    else if ( c.type == "timestamp without time zone" )
+    {
+      // by default postgresql would return date/time as a formatted string
+      // e.g. "2020-07-13 16:17:54" but we want IS0-8601 format "2020-07-13T16:17:54Z"
+      columns += "to_char(" + name + ",'YYYY-MM-DD\"T\"HH24:MI:SS\"Z\"')";
+    }
     else
       columns += name;
   }
