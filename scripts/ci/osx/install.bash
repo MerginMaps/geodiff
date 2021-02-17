fb7c4b1fdcfb7b385c6cc966ea03bb4d54e57430
Martin Dobias <wonder.sk@gmail.com>
2021-02-17 18:37:42 +0100

    travis/mac debugging python

--- scripts/ci/osx/install.bash
+++ scripts/ci/osx/install.bash
@@ -9,11 +9,18 @@ sudo python3 -m pip install nose2
 
 brew install ninja
 brew install pyenv
+
+echo "%%% before pyenv install"
+which python3
+python3 --version
+
 pyenv install 3.8.7
 pyenv install 3.7.4
 pyenv install 3.6.8
 
+echo "%%% after pyenv install"
 which python3
+python3 --version
 
 if [ -n "$TRAVIS_TAG" ]; then
     sudo python3 -m pip install twine
