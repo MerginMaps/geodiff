10e95fc4d80cde2142a9122abf02797bcfa51ad5
Martin Dobias <wonder.sk@gmail.com>
2021-04-27 09:00:10 +0200

    Fix unicode filename issue in listChanges/listChangesSummary (fixes #113)

---
+++ geodiff/tests/test_utils.cpp
@@ -0,0 +1,34 @@
+/*
+ GEODIFF - MIT License
+ Copyright (C) 2021 Martin Dobias
+*/
+
+#include "gtest/gtest.h"
+#include "geodiff_testutils.hpp"
+#include "geodiff.h"
+
+#include "geodiffutils.hpp"
+
+TEST( UtilsTest, test_unicode )
+{
+  std::string diffSource = pathjoin( testdir(), "utf_test_ščé", "changes.diff" );
+  std::string json = pathjoin( tmpdir(), "čúčo.json" );
+
+  // test whether unicode characters are working
+  EXPECT_EQ( GEODIFF_listChanges( diffSource.c_str(), json.c_str() ), GEODIFF_SUCCESS );
+
+  // make sure our test util functions can deal with unicode
+  EXPECT_TRUE( fileContains( json, "geodiff" ) );
+
+  printFileToStdout( "CUCO.JSON", json );
+}
+
+
+int main( int argc, char **argv )
+{
+  testing::InitGoogleTest( &argc, argv );
+  init_test();
+  int ret =  RUN_ALL_TESTS();
+  finalize_test();
+  return ret;
+}
