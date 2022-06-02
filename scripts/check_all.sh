b7eb5f179c84e06c72f888055681855e3fc05c93
PeterPetrik <zilolv@gmail.com>
2022-06-02 15:18:14 +0200

    revert json.h

--- scripts/check_all.sh
+++ scripts/check_all.sh
@@ -1,6 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PWD=`pwd`
 cd $DIR
-CI=0 ./run_astyle.sh `find ../geodiff -name \*.h* -print -o -name \*.c* -print`
+CI=0 ./run_astyle.sh `find ../geodiff -name \*.h* ! -path "*/3rdparty/*" -print -o -name \*.c* ! -path "*/3rdparty/*" -print`
 ./run_cppcheck.sh
 cd $PWD
