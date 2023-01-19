479b0bf8cf48468c2fa94dd70dc19b5e95bdb2f6
Peter Petrik <zilolv@gmail.com>
2023-01-19 08:27:45 +0100

    add black to CI

--- scripts/run_black.sh
+++ scripts/run_black.sh
@@ -21,13 +21,10 @@ $BLACK --version
 
 echo "running run_black for $SCRIPT_DIR/../pygeodiff"
 
-OPTIONS=$(cat <<-END
---verbose
-END
-)
-
-$BLACK $SCRIPT_DIR/../pygeodiff --verbose
-
+$BLACK $SCRIPT_DIR/../pygeodiff --check --quiet
 RETURN=$?
+
+# this does not return 0 or 1 exit code
+$BLACK $SCRIPT_DIR/../pygeodiff --verbose 
 
 exit $RETURN
