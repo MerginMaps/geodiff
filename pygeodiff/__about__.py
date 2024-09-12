db6ed20db9b66c4c11ed2eea6a9c2132b6a6b6f2
Peter Petrik <zilolv@gmail.com>
2024-09-12 10:48:07 +0200

    bump github workflow versions (#216)
    * bump minor version
    * compile sqlite on macOS outside brew (recommended in cibuildwheel docs)
    * add python 3.12 and 3.13

--- pygeodiff/__about__.py
+++ pygeodiff/__about__.py
@@ -2,7 +2,7 @@ __title__ = "PyGeoDiff"
 __description__ = "Diff tool for geo-spatial data"
 __url__ = "https://github.com/MerginMaps/geodiff"
 # use scripts/update_version.py to update the version here and in other places at once
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 __author__ = "Lutra Consulting Ltd."
 __author_email__ = "info@merginmaps.com"
 __maintainer__ = "Lutra Consulting Ltd."
