be0f51ea1e28a1f221dd23c8f7f74f98401ea275
David Koňařík <dvdkon@konarici.cz>
2025-11-05 19:40:44 +0100

    Update check_all to run Black, also clean up find cmdline

--- scripts/check_all.sh
+++ scripts/check_all.sh
@@ -1,6 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PWD=`pwd`
 cd $DIR
-CI=0 ./run_astyle.sh `find ../geodiff -name \*.h* ! -path "*/3rdparty/*" -print -o -name \*.c* ! -path "*/3rdparty/*" -print`
+CI=0 ./run_astyle.sh `find ../geodiff/{src,tests} ! -path '*/3rdparty/*' '(' -name \*.h* -o -name \*.c* ')' -print`
 ./run_cppcheck.sh
+./run_black.sh
 cd $PWD
