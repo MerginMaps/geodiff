310969d559569582c85c47abd9bd93dfccb4fad4
David Koňařík <dvdkon@konarici.cz>
2025-11-26 21:18:40 +0100

    Adapt DB constraint tests for PostgreSQL

--- pygeodiff/tests/test_rebase_db_constraint.py
+++ pygeodiff/tests/test_rebase_db_constraint.py
@@ -17,18 +17,33 @@ import json
 import os
 from pathlib import Path
 import re
-import sqlite3
 from typing import Tuple
 
 import pytest
 
 import pygeodiff
 from pygeodiff import GeoDiffLibError
+from pygeodiff.tests.testutils import GeoDiffTestSqlDb
 
 GEODIFFLIB = os.environ.get("GEODIFFLIB", None)
 
 # These tests cover simple rebase scenarios, with or without database constraints
 # applied.
+
+
+driver_parametrize = pytest.mark.parametrize(
+    "driver",
+    [
+        "sqlite",
+        pytest.param(
+            "postgres",
+            marks=pytest.mark.skipif(
+                "GEODIFF_PG_CONNINFO" not in os.environ,
+                reason="Can't test Postgres without GEODIFF_PG_CONNINFO env variable",
+            ),
+        ),
+    ],
+)
 
 
 @pytest.mark.parametrize(
@@ -41,8 +56,9 @@ GEODIFFLIB = os.environ.get("GEODIFFLIB", None)
 @pytest.mark.parametrize(
     "user_a_data_first", [True, False], ids=["user_a_data_first", "user_b_data_first"]
 )
+@driver_parametrize
 def test_geodiff_rebase_happy_path_single_table(
-    db_constrained, user_a_data_first, tmp_path
+    db_constrained, user_a_data_first, driver, tmp_path
 ):
     """
     This test checks that rebase succeeds on changes to a single table with
@@ -52,7 +68,10 @@ def test_geodiff_rebase_happy_path_single_table(
     # Arrange
     geodiff = pygeodiff.GeoDiff(GEODIFFLIB)
     conflict = tmp_path / "conflict.txt"
-    original, user_a, user_b = create_db_files(tmp_path, db_constrained=db_constrained)
+    diff = tmp_path / "diff.bin"
+    original, user_a, user_b = create_db_files(
+        driver, tmp_path, db_constrained=db_constrained
+    )
 
     # Apply changes to databases to give the following expected values
     expected = {
@@ -65,10 +84,10 @@ def test_geodiff_rebase_happy_path_single_table(
         ],
     }
 
-    with sqlite3.connect(user_a) as conn_a:
+    with user_a.open() as conn_a:
         conn_a.execute(
             """
-            UPDATE species SET name='Maple_updated' WHERE species_id='MPL'
+            UPDATE species SET name='Maple_updated' WHERE species_id = 'MPL'
             """
         )
         conn_a.execute(
@@ -79,10 +98,10 @@ def test_geodiff_rebase_happy_path_single_table(
             """
         )
 
-    with sqlite3.connect(user_b) as conn_b:
+    with user_b.open() as conn_b:
         conn_b.execute(
             """
-            UPDATE species SET name='Oak_updated' WHERE species_id='OAK'
+            UPDATE species SET name='Oak_updated' WHERE species_id = 'OAK'
             """
         )
         conn_b.execute(
@@ -94,12 +113,12 @@ def test_geodiff_rebase_happy_path_single_table(
         )
         conn_b.execute(
             """
-            DELETE FROM trees WHERE species_id IS 'PIN'
+            DELETE FROM trees WHERE species_id = 'PIN'
             """
         )
         conn_b.execute(
             """
-            DELETE FROM species WHERE species_id IS 'PIN'
+            DELETE FROM species WHERE species_id = 'PIN'
             """
         )
 
@@ -109,8 +128,18 @@ def test_geodiff_rebase_happy_path_single_table(
     else:
         theirs, mine = user_b, user_a
 
+    geodiff.create_changeset_ex(
+        driver, original.driver_info, original.db, theirs.db, str(diff)
+    )
     # Act (this will raise a GeoDiffLibError if geodiff cannot handle foreign keys)
-    geodiff.rebase(str(original), str(theirs), str(mine), str(conflict))
+    geodiff.rebase_ex(
+        driver,
+        original.driver_info,
+        original.db,
+        mine.db,
+        str(diff),
+        str(conflict),
+    )
 
     # Assert that rebased database contains expected changes and no conflict exists
     assert_data_as_expected(mine, expected)
@@ -127,8 +156,9 @@ def test_geodiff_rebase_happy_path_single_table(
 @pytest.mark.parametrize(
     "user_a_data_first", [True, False], ids=["user_a_data_first", "user_b_data_first"]
 )
+@driver_parametrize
 def test_geodiff_rebase_happy_path_fk_tables(
-    db_constrained, user_a_data_first, tmp_path
+    db_constrained, user_a_data_first, driver, tmp_path
 ):
     """
     This test checks that rebase succeeds on changes on multiple tables related
@@ -138,7 +168,10 @@ def test_geodiff_rebase_happy_path_fk_tables(
     # Arrange
     geodiff = pygeodiff.GeoDiff(GEODIFFLIB)
     conflict = tmp_path / "conflict.txt"
-    original, user_a, user_b = create_db_files(tmp_path, db_constrained=db_constrained)
+    diff = tmp_path / "diff.txt"
+    original, user_a, user_b = create_db_files(
+        driver, tmp_path, db_constrained=db_constrained
+    )
 
     # Apply changes to databases to give the following expected values
     expected = {
@@ -158,7 +191,7 @@ def test_geodiff_rebase_happy_path_fk_tables(
         ],
     }
 
-    with sqlite3.connect(user_a) as conn_a:
+    with user_a.open() as conn_a:
         conn_a.execute(
             """
             INSERT INTO species (species_id, name) VALUES (
@@ -175,12 +208,12 @@ def test_geodiff_rebase_happy_path_fk_tables(
         )
         conn_a.execute(
             """
-            DELETE FROM trees WHERE tree_id IS 20251103003
+            DELETE FROM trees WHERE tree_id = 20251103003
             """
         )
         conn_a.execute(
             """
-            DELETE FROM species WHERE species_id IS 'PIN'
+            DELETE FROM species WHERE species_id = 'PIN'
             """
         )
         conn_a.execute(
@@ -189,7 +222,7 @@ def test_geodiff_rebase_happy_path_fk_tables(
             """
         )
 
-    with sqlite3.connect(user_b) as conn_b:
+    with user_b.open() as conn_b:
         conn_b.execute(
             """
             INSERT INTO species (species_id, name) VALUES (
@@ -216,8 +249,18 @@ def test_geodiff_rebase_happy_path_fk_tables(
     else:
         theirs, mine = user_b, user_a
 
+    geodiff.create_changeset_ex(
+        driver, original.driver_info, original.db, theirs.db, str(diff)
+    )
     # Act (this will raise a GeoDiffLibError if geodiff cannot handle foreign keys)
-    geodiff.rebase(str(original), str(theirs), str(mine), str(conflict))
+    geodiff.rebase_ex(
+        driver,
+        original.driver_info,
+        original.db,
+        mine.db,
+        str(diff),
+        str(conflict),
+    )
 
     # Assert that rebased database contains expected changes and no conflict exists
     assert_data_as_expected(mine, expected)
@@ -231,7 +274,8 @@ def test_geodiff_rebase_happy_path_fk_tables(
 @pytest.mark.parametrize(
     "user_a_data_first", [True, False], ids=["user_a_data_first", "user_b_data_first"]
 )
-def test_geodiff_rebase_conflicting_edits(user_a_data_first, tmp_path):
+@driver_parametrize
+def test_geodiff_rebase_conflicting_edits(user_a_data_first, driver, tmp_path):
     """
     This test checks that rebase handles conflicting edits to the same row and
     column.
@@ -239,17 +283,18 @@ def test_geodiff_rebase_conflicting_edits(user_a_data_first, tmp_path):
     # Arrange
     geodiff = pygeodiff.GeoDiff(GEODIFFLIB)
     conflict = tmp_path / "conflict.txt"
-    original, user_a, user_b = create_db_files(tmp_path, db_constrained=True)
+    diff = tmp_path / "diff.txt"
+    original, user_a, user_b = create_db_files(driver, tmp_path, db_constrained=True)
 
     # Apply changes to databases (both users edit same value)
-    with sqlite3.connect(user_a) as conn_a:
+    with user_a.open() as conn_a:
         conn_a.execute(
             """
             UPDATE species SET name='Maple_A' WHERE species_id='MPL'
             """
         )
 
-    with sqlite3.connect(user_b) as conn_b:
+    with user_b.open() as conn_b:
         conn_b.execute(
             """
             UPDATE species SET name='Maple_B' WHERE species_id='MPL'
@@ -278,8 +323,13 @@ def test_geodiff_rebase_conflicting_edits(user_a_data_first, tmp_path):
             ]
         }
 
-    # Act
-    geodiff.rebase(str(original), str(theirs), str(mine), str(conflict))
+    geodiff.create_changeset_ex(
+        driver, original.driver_info, original.db, theirs.db, str(diff)
+    )
+    # Act (this will raise a GeoDiffLibError if geodiff cannot handle foreign keys)
+    geodiff.rebase_ex(
+        driver, original.driver_info, original.db, mine.db, str(diff), str(conflict)
+    )
 
     # Assert that rebased database contains expected changes and conflict is recorded
     assert_data_as_expected(mine, expected)
@@ -292,7 +342,10 @@ def test_geodiff_rebase_conflicting_edits(user_a_data_first, tmp_path):
 @pytest.mark.parametrize(
     "user_a_data_first", [True, False], ids=["user_a_data_first", "user_b_data_first"]
 )
-def test_geodiff_rebase_unique_constraint_violation(user_a_data_first, tmp_path):
+@driver_parametrize
+def test_geodiff_rebase_unique_constraint_violation(
+    user_a_data_first, driver, tmp_path
+):
     """
     This test covers a rebase where combining edits causes a unique constraint
     violation.
@@ -300,10 +353,11 @@ def test_geodiff_rebase_unique_constraint_violation(user_a_data_first, tmp_path)
     # Arrange
     geodiff = pygeodiff.GeoDiff(GEODIFFLIB)
     conflict = tmp_path / "conflict.txt"
-    original, user_a, user_b = create_db_files(tmp_path, db_constrained=True)
+    diff = tmp_path / "diff.txt"
+    original, user_a, user_b = create_db_files(driver, tmp_path, db_constrained=True)
 
     # Apply changes to databases (both users create the same species_id)
-    with sqlite3.connect(user_a) as conn_a:
+    with user_a.open() as conn_a:
         conn_a.execute(
             """
             INSERT INTO species (species_id, name) VALUES (
@@ -312,7 +366,7 @@ def test_geodiff_rebase_unique_constraint_violation(user_a_data_first, tmp_path)
             """
         )
 
-    with sqlite3.connect(user_b) as conn_b:
+    with user_b.open() as conn_b:
         conn_b.execute(
             """
             INSERT INTO species (species_id, name) VALUES (
@@ -327,15 +381,21 @@ def test_geodiff_rebase_unique_constraint_violation(user_a_data_first, tmp_path)
     else:
         theirs, mine = user_b, user_a
 
+    geodiff.create_changeset_ex(
+        driver, original.driver_info, original.db, theirs.db, str(diff)
+    )
     # Assert that rebasing fails due to DB constraints on application of diffs
     with pytest.raises(GeoDiffLibError):
-        geodiff.rebase(str(original), str(theirs), str(mine), str(conflict))
+        geodiff.rebase_ex(
+            driver, original.driver_info, original.db, mine.db, str(diff), str(conflict)
+        )
 
 
 @pytest.mark.parametrize(
     "user_a_data_first", [True, False], ids=["user_a_data_first", "user_b_data_first"]
 )
-def test_geodiff_rebase_fkey_constraint_violation(user_a_data_first, tmp_path):
+@driver_parametrize
+def test_geodiff_rebase_fkey_constraint_violation(user_a_data_first, driver, tmp_path):
     """
     This test covers a rebase where combining edits causes a foreign key constraint
     violation.
@@ -346,23 +406,24 @@ def test_geodiff_rebase_fkey_constraint_violation(user_a_data_first, tmp_path):
     # Arrange
     geodiff = pygeodiff.GeoDiff(GEODIFFLIB)
     conflict = tmp_path / "conflict.txt"
-    original, user_a, user_b = create_db_files(tmp_path, db_constrained=True)
+    diff = tmp_path / "diff.txt"
+    original, user_a, user_b = create_db_files(driver, tmp_path, db_constrained=True)
 
     # Apply changes to databases (user_a deletes species_id for tree inserted by user_b)
-    with sqlite3.connect(user_a) as conn_a:
+    with user_a.open() as conn_a:
         # Delete pine trees and parent pine species
         conn_a.execute(
             """
-            DELETE FROM trees WHERE species_id IS 'PIN'
+            DELETE FROM trees WHERE species_id = 'PIN'
             """
         )
         conn_a.execute(
             """
-            DELETE FROM species WHERE species_id IS 'PIN'
+            DELETE FROM species WHERE species_id = 'PIN'
             """
         )
 
-    with sqlite3.connect(user_b) as conn_b:
+    with user_b.open() as conn_b:
         # Add a pine tree
         conn_b.execute(
             """
@@ -378,20 +439,25 @@ def test_geodiff_rebase_fkey_constraint_violation(user_a_data_first, tmp_path):
     else:
         theirs, mine = user_b, user_a
 
+    geodiff.create_changeset_ex(
+        driver, original.driver_info, original.db, theirs.db, str(diff)
+    )
     # Assert that rebasing fails due to DB constraints on application of diffs
     with pytest.raises(GeoDiffLibError):
-        geodiff.rebase(str(original), str(theirs), str(mine), str(conflict))
+        geodiff.rebase_ex(
+            driver, original.driver_info, original.db, mine.db, str(diff), str(conflict)
+        )
 
 
 # Helper functions are defined below here.
 
 
-def assert_data_as_expected(db: Path, expected_data: dict[str, list[dict]]):
+def assert_data_as_expected(db: GeoDiffTestSqlDb, expected_data: dict[str, list[dict]]):
     """
     Assert that table contents are as expected.  Use `set` comparison because
     the row ordering depends on the order that tables are passed to `rebase`.
     """
-    with sqlite3.connect(db) as conn:
+    with db.open() as conn:
         for table, rows in expected_data.items():
             column_names = rows[0].keys()
             results = conn.execute(
@@ -402,33 +468,43 @@ def assert_data_as_expected(db: Path, expected_data: dict[str, list[dict]]):
 
 
 def create_db_files(
-    tmp_path: Path, db_constrained: bool = True
-) -> Tuple[Path, Path, Path]:
+    driver: str, tmp_path: Path, db_constrained: bool = True
+) -> Tuple[GeoDiffTestSqlDb, GeoDiffTestSqlDb, GeoDiffTestSqlDb]:
     """
-    Create 3 SQLite files at the given filepaths, each with the same tables
-    and data.
+    Create 3 Postgres schemata or SQLite files at the given filepaths, each
+    filled with the same data.
 
     If `db_constrained`, the database will apply UNIQUE and FOREIGN
     KEY constraints.
     """
-    original = tmp_path / "original.db"
-    user_a = tmp_path / "user_a.db"
-    user_b = tmp_path / "user_b.db"
+
+    if driver == "sqlite":
+        original = tmp_path / "original.db"
+        user_a = tmp_path / "user_a.db"
+        user_b = tmp_path / "user_b.db"
+
+        pkey_type = "integer"
+    elif driver == "postgres":
+        original = "original"
+        user_a = "user_a"
+        user_b = "user_b"
+
+        pkey_type = "serial"
 
     # Define tables
-    create_species_sql = """
+    create_species_sql = f"""
         CREATE TABLE species (
-            fid INTEGER PRIMARY KEY,
+            fid {pkey_type} PRIMARY KEY DEFERRABLE,
             species_id TEXT UNIQUE,
             name TEXT
         )"""
-    create_trees_sql = """
+    create_trees_sql = f"""
         CREATE TABLE trees (
-            fid INTEGER PRIMARY KEY,
-            tree_id INTEGER UNIQUE NOT NULL,
+            fid {pkey_type} PRIMARY KEY DEFERRABLE,
+            tree_id BIGINT UNIQUE DEFERRABLE NOT NULL,
             species_id TEXT NOT NULL,
             age INTEGER,
-            FOREIGN KEY("species_id") REFERENCES "species"("species_id")
+            FOREIGN KEY("species_id") REFERENCES "species"("species_id") DEFERRABLE
         )"""
 
     if not db_constrained:
@@ -438,6 +514,11 @@ def create_db_files(
         create_trees_sql = re.sub(
             r",\W*FOREIGN.*REFERENCES.*?\)", "", create_trees_sql, re.MULTILINE
         )
+
+    if driver == "sqlite" or not db_constrained:
+        # Remove DEFERRABLE specifier
+        create_species_sql = re.sub(r"\bDEFERRABLE\b", "", create_species_sql)
+        create_trees_sql = re.sub(r"\bDEFERRABLE\b", "", create_trees_sql)
 
     # Define initial data
     species_data = [
@@ -451,26 +532,27 @@ def create_db_files(
         {"tree_id": 20251103003, "species_id": "PIN", "age": 18},
     ]
 
-    # Create geopackages
-    with sqlite3.connect(original) as conn:
-        conn.execute(create_species_sql)
-        conn.execute(create_trees_sql)
-        conn.executemany(
-            """
-            INSERT INTO species (species_id, name)
-            VALUES (:species_id, :name)
-            """,
-            species_data,
-        )
-        conn.executemany(
-            """
-            INSERT INTO trees (tree_id, species_id, age)
-            VALUES (:tree_id, :species_id, :age)
-            """,
-            trees_data,
-        )
+    # Create databases
+    dbs = []
+    for dbname in [original, user_a, user_b]:
+        db = GeoDiffTestSqlDb(driver, dbname, destroy_existing=True)
+        dbs.append(db)
+        with db.open() as conn:
+            conn.execute(create_species_sql)
+            conn.execute(create_trees_sql)
+            conn.executemany(
+                """
+                INSERT INTO species (species_id, name)
+                VALUES (:species_id, :name)
+                """,
+                species_data,
+            )
+            conn.executemany(
+                """
+                INSERT INTO trees (tree_id, species_id, age)
+                VALUES (:tree_id, :species_id, :age)
+                """,
+                trees_data,
+            )
 
-    user_a.write_bytes(original.read_bytes())
-    user_b.write_bytes(original.read_bytes())
-
-    return original, user_a, user_b
+    return tuple(dbs)
