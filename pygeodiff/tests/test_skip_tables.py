7d078fca25788a4b7e07dc7efa4abd0b789ac9f3
Jan Caha <jan.caha@outlook.com>
2026-06-11 13:49:10 +0200

    add tests

--- pygeodiff/tests/test_skip_tables.py
+++ pygeodiff/tests/test_skip_tables.py
@@ -99,3 +99,83 @@ class UnitTestsPythonSingleCommit(GeoDiffTests):
         check_nchanges(self.geodiff, changeset2, 0)
 
         self.geodiff.set_tables_to_skip([])
+
+    def test_include_create(self):
+        base = geodiff_test_dir() + "/" + "skip_tables" + "/" + "base.gpkg"
+        modified = geodiff_test_dir() + "/" + "skip_tables" + "/" + "modified_all.gpkg"
+        changeset = tmpdir() + "/py" + "test_include_create" + "/" + "changeset_points.bin"
+        changeset2 = (
+            tmpdir() + "/py" + "test_include_create" + "/" + "changeset_points2.bin"
+        )
+        changeset_inv = (
+            tmpdir() + "/py" + "test_include_create" + "/" + "changeset_inv.bin"
+        )
+        patched = tmpdir() + "/py" + "test_include_create" + "/" + "patched_points.gpkg"
+
+        create_dir("test_include_create")
+
+        # include only points table when creating changeset (lines changes are ignored)
+        self.geodiff.set_tables_to_include(["points"])
+
+        # create changeset
+        self.geodiff.create_changeset(base, modified, changeset)
+        check_nchanges(self.geodiff, changeset, 4)
+
+        # apply changeset
+        shutil.copyfile(base, patched)
+        self.geodiff.apply_changeset(patched, changeset)
+
+        # check that now it is same file (for included tables)
+        self.geodiff.create_changeset(patched, modified, changeset2)
+        check_nchanges(self.geodiff, changeset2, 0)
+
+        # check we can create inverted changeset
+        os.remove(changeset2)
+        self.geodiff.invert_changeset(changeset, changeset_inv)
+        self.geodiff.apply_changeset(patched, changeset_inv)
+        self.geodiff.create_changeset_dr(
+            "sqlite", "", patched, "sqlite", "", base, changeset2
+        )
+        check_nchanges(self.geodiff, changeset2, 0)
+
+        self.geodiff.set_tables_to_include([])
+
+    def test_include_apply(self):
+        base = geodiff_test_dir() + "/" + "skip_tables" + "/" + "base.gpkg"
+        modified = geodiff_test_dir() + "/" + "skip_tables" + "/" + "modified_all.gpkg"
+        changeset = tmpdir() + "/py" + "test_include_apply" + "/" + "changeset_points.bin"
+        changeset2 = (
+            tmpdir() + "/py" + "test_include_apply" + "/" + "changeset_points2.bin"
+        )
+        changeset_inv = (
+            tmpdir() + "/py" + "test_include_apply" + "/" + "changeset_inv.bin"
+        )
+        patched = tmpdir() + "/py" + "test_include_apply" + "/" + "patched_points.gpkg"
+
+        create_dir("test_include_apply")
+
+        # create changeset without filter (captures all 6 changes)
+        self.geodiff.create_changeset(base, modified, changeset)
+        check_nchanges(self.geodiff, changeset, 6)
+
+        # include only points table when applying changeset
+        self.geodiff.set_tables_to_include(["points"])
+
+        # apply changeset
+        shutil.copyfile(base, patched)
+        self.geodiff.apply_changeset(patched, changeset)
+
+        # check that now it is same file (for included tables)
+        self.geodiff.create_changeset(patched, modified, changeset2)
+        check_nchanges(self.geodiff, changeset2, 0)
+
+        # check we can create inverted changeset
+        os.remove(changeset2)
+        self.geodiff.invert_changeset(changeset, changeset_inv)
+        self.geodiff.apply_changeset(patched, changeset_inv)
+        self.geodiff.create_changeset_dr(
+            "sqlite", "", patched, "sqlite", "", base, changeset2
+        )
+        check_nchanges(self.geodiff, changeset2, 0)
+
+        self.geodiff.set_tables_to_include([])
