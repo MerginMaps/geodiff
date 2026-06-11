0cc6662ae53af1bb6d24bdd5c991d41e77d59e7f
Jan Caha <jan.caha@outlook.com>
2026-06-11 11:39:28 +0200

    add setTablesToInclude to Context along with checks that is not set at the same time as tables to skip

---
+++ geodiff/tests/test_context.cpp
@@ -0,0 +1,71 @@
+/*
+ GEODIFF - MIT License
+ Copyright (C) 2026 Jan Caha
+*/
+
+#include "gtest/gtest.h"
+#include "geodiffcontext.hpp"
+#include "geodiffutils.hpp"
+
+TEST( ContextTest, defaultFilterModeIsNothing )
+{
+  Context ctx;
+  EXPECT_EQ( ctx.tableFilterMode(), TablesFilterMode::Nothing );
+}
+
+TEST( ContextTest, setTablesToSkip )
+{
+  Context ctx;
+  ctx.setTablesToSkip( { "lines", "polygons" } );
+  EXPECT_EQ( ctx.tableFilterMode(), TablesFilterMode::SkippedTables );
+}
+
+TEST( ContextTest, setTablesToInclude )
+{
+  Context ctx;
+  ctx.setTablesToInclude( { "points" } );
+  EXPECT_EQ( ctx.tableFilterMode(), TablesFilterMode::IncludedTables );
+}
+
+TEST( ContextTest, cannotSetIncludeAfterSkip )
+{
+  Context ctx;
+  ctx.setTablesToSkip( { "lines" } );
+  EXPECT_THROW( ctx.setTablesToInclude( { "points" } ), GeoDiffException );
+}
+
+TEST( ContextTest, cannotSetSkipAfterInclude )
+{
+  Context ctx;
+  ctx.setTablesToInclude( { "points" } );
+  EXPECT_THROW( ctx.setTablesToSkip( { "lines" } ), GeoDiffException );
+}
+
+TEST( ContextTest, skipModeSkipsListedTable )
+{
+  Context ctx;
+  ctx.setTablesToSkip( { "lines" } );
+  EXPECT_TRUE( ctx.isTableSkipped( "lines" ) );
+  EXPECT_FALSE( ctx.isTableSkipped( "points" ) );
+}
+
+TEST( ContextTest, includeModeSkipsUnlistedTable )
+{
+  Context ctx;
+  ctx.setTablesToInclude( { "points" } );
+  EXPECT_FALSE( ctx.isTableSkipped( "points" ) );
+  EXPECT_TRUE( ctx.isTableSkipped( "lines" ) );
+}
+
+TEST( ContextTest, nothingModeSkipsNothing )
+{
+  Context ctx;
+  EXPECT_FALSE( ctx.isTableSkipped( "points" ) );
+  EXPECT_FALSE( ctx.isTableSkipped( "lines" ) );
+}
+
+int main( int argc, char **argv )
+{
+  testing::InitGoogleTest( &argc, argv );
+  return RUN_ALL_TESTS();
+}
